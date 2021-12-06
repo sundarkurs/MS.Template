cab72fa6b90a8c337e3f9a540446eb1eb9c8e5ee
Sundar Urs <sundarkurs@gmail.com>
2021-12-06 16:42:48 +0530

    Added test projects

---
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Smoke.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WSA.Microservice.Template.Smoke.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
