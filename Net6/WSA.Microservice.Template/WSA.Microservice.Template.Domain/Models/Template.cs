251aef23be923bed5bd1c4e04440fe4291cc5adf
Sundar Urs <sundarkurs@gmail.com>
2021-12-07 17:22:32 +0530

    db context and entities have been added

---
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Domain/Models/Template.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WSA.Microservice.Template.Domain.Models
+{
+    public partial class Template
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- Net6/WSA.Microservice.Template/WSA.Microservice.Template.Persistence/Contexts/TemplateContext.cs
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Persistence/Contexts/TemplateContext.cs
@@ -12,5 +12,39 @@ namespace WSA.Microservice.Template.Persistence.Contexts
             : base(options)
         {
         }
+
+        public virtual DbSet<Domain.Models.Template> Templates { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tst-identity-eu-n-wdx-sql.database.windows.net;Database=tst-ms-template-sdb;Trusted_Connection=False;Encrypt=True;uid=widex_db@tst-identity-eu-n-wdx-sql;password=W!dex123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Models.Template>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Template");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
