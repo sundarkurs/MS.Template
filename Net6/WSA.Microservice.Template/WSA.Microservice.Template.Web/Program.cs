b435dfd78df4d230f1ae5e505265db58d6a4261b
Sundar Urs <sundarkurs@gmail.com>
2021-12-07 15:05:54 +0530

    Update Program.cs

--- Net6/WSA.Microservice.Template/WSA.Microservice.Template.Web/Program.cs
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Web/Program.cs
@@ -10,91 +10,100 @@ using WSA.Microservice.Template.Web.Logger;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-builder.Configuration.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
+ConfigureConfiguration(builder.Configuration);
 
-
-var provider = builder.Services.BuildServiceProvider();
-var configuration = provider.GetRequiredService<IConfiguration>();
-
-
-/**** Add services to the container. ****/
-
-builder.Services.AddSingleton<ITelemetryInitializer>(new CloudRoleNameTelemetryInitializer(configuration.GetValue<string>("appInsights:WebApp:CloudRoleName")));
-builder.Services.AddApplicationInsightsTelemetry(options =>
-{
-    options.InstrumentationKey = configuration.GetValue<string>("appInsights:iKey");
-});
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-});
-
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-     .AddMicrosoftIdentityWebApp(options =>
-     {
-         configuration.Bind("Authentication:AzureAdB2C", options);
-         options.Events.OnRedirectToIdentityProvider += OnRedirectToIdentityProvider;
-         options.Events.OnTokenValidated += OnTokenValidated;
-         options.Events.OnRemoteFailure += OnRemoteFailure;
-     });
-
-Task OnRemoteFailure(RemoteFailureContext arg)
-{
-    throw new NotImplementedException();
-}
-
-builder.Services.AddAuthorization(options =>
-{
-    //if (Configuration.GetValue<bool>("EnableTokenPage"))
-    //    options.AddPolicy("AllowTokenView", policy => policy.RequireClaim("Role", "AllowTokenView"));
-    options.AddPolicy("AllowTokenView", policy => policy.Requirements.Add(new ViewTokenRequirement(configuration.GetValue<bool>("EnableTokenPage"))));
-});
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
-builder.Services.AddSingleton<IAuthorizationHandler, ViewTokenAuthorizationHandler>();
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-/**** Configure the HTTP request pipeline. ****/
+ConfigureMiddleware(app);
 
-app.Use((context, next) =>
-{
-    context.Request.Scheme = "https";
-    return next();
-});
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseCookiePolicy();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
+ConfigureEndpoints(app);
 
 app.Run();
 
 
+#region Configure Methods
 
+void ConfigureConfiguration(ConfigurationManager configuration)
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
+    configuration.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
+}
+
+void ConfigureServices(IServiceCollection services, ConfigurationManager configuration)
+{
+    services.AddSingleton<ITelemetryInitializer>(new CloudRoleNameTelemetryInitializer(configuration.GetValue<string>("appInsights:WebApp:CloudRoleName")));
+    services.AddApplicationInsightsTelemetry(options =>
+    {
+        options.InstrumentationKey = configuration.GetValue<string>("appInsights:iKey");
+    });
+    services.Configure<CookiePolicyOptions>(options =>
+    {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+    });
+
+    services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+         .AddMicrosoftIdentityWebApp(options =>
+         {
+             configuration.Bind("Authentication:AzureAdB2C", options);
+             options.Events.OnRedirectToIdentityProvider += OnRedirectToIdentityProvider;
+             options.Events.OnTokenValidated += OnTokenValidated;
+             options.Events.OnRemoteFailure += OnRemoteFailure;
+         });
+
+    services.AddAuthorization(options =>
+    {
+        //if (Configuration.GetValue<bool>("EnableTokenPage"))
+        //    options.AddPolicy("AllowTokenView", policy => policy.RequireClaim("Role", "AllowTokenView"));
+        options.AddPolicy("AllowTokenView", policy => policy.Requirements.Add(new ViewTokenRequirement(configuration.GetValue<bool>("EnableTokenPage"))));
+    });
+
+    services.AddControllersWithViews();
+    services.AddRazorPages().AddMicrosoftIdentityUI();
+    services.AddSingleton<IAuthorizationHandler, ViewTokenAuthorizationHandler>();
+};
+
+void ConfigureMiddleware(WebApplication app)
+{
+    app.Use((context, next) =>
+    {
+        context.Request.Scheme = "https";
+        return next();
+    });
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseCookiePolicy();
+    app.UseAuthentication();
+    app.UseAuthorization();
+};
+
+void ConfigureEndpoints(WebApplication app)
+{
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+};
+
+#endregion
+
+#region Azure Authentication Handlers
 
 Task OnRedirectToIdentityProvider(RedirectContext context)
 {
@@ -116,3 +125,10 @@ Task OnTokenValidated(TokenValidatedContext context)
     context.Principal.AddIdentity(identity);
     return Task.CompletedTask;
 }
+
+Task OnRemoteFailure(RemoteFailureContext arg)
+{
+    throw new NotImplementedException();
+}
+
+#endregion
