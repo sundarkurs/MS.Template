f28f129fd8319c244f4b221c104dc6bbad66c2b5
Sundar Urs <sundarkurs@gmail.com>
2021-12-10 11:32:37 +0530

    Added xUnit projects

--- Net6/WSA.Microservice.Template/WSA.Microservice.Template.Web.Tests/UnitTest1.cs
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Web.Tests/UnitTest1.cs
@@ -1,18 +1,13 @@
-using NUnit.Framework;
+using Xunit;
 
 namespace WSA.Microservice.Template.Web.Tests
 {
-    public class Tests
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
+        [Fact]
         public void Test1()
         {
-            Assert.Pass();
+
         }
     }
 }
