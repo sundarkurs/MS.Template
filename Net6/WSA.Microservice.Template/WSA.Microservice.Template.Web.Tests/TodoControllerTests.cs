b34459482d839a7b32c7633373498beddd0647fe
Sundar Urs <sundarkurs@gmail.com>
2021-12-16 15:14:10 +0530

    cleanup

--- Net6/WSA.Microservice.Template/WSA.Microservice.Template.Web.Tests/TodoControllerTests.cs
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Web.Tests/TodoControllerTests.cs
@@ -1,8 +1,5 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Moq;
 using System.Threading.Tasks;
-using WSA.Microservice.Template.Web.Controllers.Api;
 using Xunit;
 
 namespace WSA.Microservice.Template.Web.Tests
