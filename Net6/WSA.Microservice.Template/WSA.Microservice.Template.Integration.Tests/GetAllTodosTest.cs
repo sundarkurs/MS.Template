b4186486b9d1f944e5e4a92bb9d8e320c6f4cce5
Sundar Urs <sundarkurs@gmail.com>
2021-12-17 15:26:40 +0530

    Update GetAllTodosTest.cs

--- Net6/WSA.Microservice.Template/WSA.Microservice.Template.Integration.Tests/GetAllTodosTest.cs
+++ Net6/WSA.Microservice.Template/WSA.Microservice.Template.Integration.Tests/GetAllTodosTest.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using MediatR;
 using Moq;
+using System.Collections.Generic;
+using System.Linq;
 using WSA.Microservice.Template.Application.Common.Interfaces.Repositories;
 using WSA.Microservice.Template.Application.Queries.Todo;
 using Xunit;
@@ -9,22 +11,44 @@ namespace WSA.Microservice.Template.Integration.Tests
 {
     public class GetAllTodosTest
     {
+        private static IMapper _mapper;
+
+        public GetAllTodosTest()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new Application.Common.Mappings.ApplicationProfile());
+                });
+                _mapper = mappingConfig.CreateMapper();
+            }
+        }
+
         [Fact]
         public async void GetAllTodos_ReturnsAllTodos()
         {
-            var mediator = new Mock<IMediator>();
+            // Arrange
+
+            var entities = new List<Domain.Entities.Todo> { new Domain.Entities.Todo { Id = 1, Title = "Test", Description = "Test description" } };
 
             var todoRepo = new Mock<ITodoRepository>();
 
-            var mapper = new Mock<IMapper>();
+            todoRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(entities);
 
             var query = new GetAllTodos.Query();
 
-            var handler = new GetAllTodos.Handler(todoRepo.Object, mapper.Object);
+            var handler = new GetAllTodos.Handler(todoRepo.Object, _mapper);
 
-            var resp = await handler.Handle(query, new System.Threading.CancellationToken());
+            // Act
+            var response = await handler.Handle(query, new System.Threading.CancellationToken());
 
-            Assert.NotNull(resp);
+            // Assert
+
+            Assert.NotNull(response);
+            Assert.True(response.Succeeded);
+            Assert.NotNull(response.Data);
+            Assert.True(response.Data.ToList().Count > 0);
 
         }
     }
